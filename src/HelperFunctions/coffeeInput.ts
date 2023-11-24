454b14df9c05ada955c30a1906c7fd50d19d76ec
Hannah Thorley <hannahthorley2014@gmail.com>
2023-11-24 14:25:44 +0000

    coffeeinput

--- src/HelperFunctions/coffeeInput.ts
+++ src/HelperFunctions/coffeeInput.ts
@@ -1,1 +1,24 @@
-//takes user input, inserts it into ‘coffeeInput’ function that multiplies input by amount of miligrams of caffeine in a coffee outputs initialAmount (type number) to be inserted into equation.ts
+// takes user input, inserts it into ‘coffeeInput’ function that multiplies input by amount of miligrams of caffeine in a coffee outputs initialAmount (type number) to be inserted into equation.ts
+// type  myCoffee = numberOfCoffees: number | null;
+
+function coffeeInput(): number | void {
+  let numberOfCoffees: string = "";
+  let coffees = prompt(
+    "How many coffees have you had today? Please enter a number",
+    numberOfCoffees
+  );
+  if (coffees !== null) {
+    const numberOfCoffees = parseInt(coffees, 10);
+
+    if (!isNaN(numberOfCoffees)) {
+      const caffeine: number = Math.floor(numberOfCoffees * 90);
+      console.log(caffeine);
+      return caffeine;
+    }
+  } else {
+    console.error("Invalid input. Please enter a valid number.");
+  }
+}
+coffeeInput();
+
+export default coffeeInput;
